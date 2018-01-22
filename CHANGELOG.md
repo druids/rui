621b975a2b96d936b09769b958bc8213b0cbfcb3
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-01-22 16:04:30 +0100

    0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,9 @@
+## 0.1.1
+
+#### Fixed
+
+  - Fixed #5 :source-paths in project.clj
+
 ## 0.1.0
 
 #### New Features
--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject druids/rui "0.1.0"
+(defproject druids/rui "0.1.1"
   :description "UI components based on Reagent and Re-frame"
   :url "https://github.com/druids/rui"
   :license {:name "MIT License"
