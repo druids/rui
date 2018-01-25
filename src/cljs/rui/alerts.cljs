ac7806bd3a7a11720eca5dafdc25164f3b473cb4
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-01-25 11:42:22 +0100

    Added API file for alerts module #10

---
+++ src/cljs/rui/alerts.cljs
@@ -0,0 +1,14 @@
+(ns rui.alerts
+  (:require
+    [rui.alerts.components :as components]))
+
+
+(def alert components/alert)
+(def alert-primary components/alert-primary)
+(def alert-secondary components/alert-secondary)
+(def alert-success components/alert-success)
+(def alert-danger components/alert-danger)
+(def alert-warning components/alert-warning)
+(def alert-info components/alert-info)
+(def alert-light components/alert-light)
+(def alert-dark components/alert-dark)
